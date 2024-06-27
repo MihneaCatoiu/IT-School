eb854ad6ca355347947a70484652190228d6e4e8
MihneaCatoiu <mihneaaureliancatoiu@gmail.com>
2024-06-27 18:21:44 +0300

    Provocari 27.06.2024 (PARTIAL).

---
+++ src/main/java/session_7_utility_classes/challenge/challenge1.java
@@ -0,0 +1,10 @@
+package session_7_utility_classes.challenge;
+
+import java.time.LocalDate;
+
+public class challenge1 {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+    }
+}
---
+++ src/main/java/session_7_utility_classes/challenge/challenge2.java
@@ -0,0 +1,29 @@
+package session_7_utility_classes.challenge;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class challenge2 {
+    public static void displayDateComponents(String dateStr) {
+        LocalDate date = LocalDate.parse(dateStr);
+
+        int year = date.getYear();
+        int month = date.getMonthValue();
+        int day = date.getDayOfMonth();
+
+        System.out.println("Year: " + year);
+        System.out.println("Month: " + String.format("%02d", month));
+        System.out.println("Day: " + String.format("%02d", day));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a date (YYYY-MM-DD): ");
+        String dateStr = scanner.nextLine();
+
+        displayDateComponents(dateStr);
+
+        scanner.close();
+    }
+}
