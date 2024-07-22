37a37a9f419eef00e0239315b301f75971090153
MihneaCatoiu <mihneaaureliancatoiu@gmail.com>
2024-07-22 21:14:53 +0300

    Session 15.

---
+++ src/main/java/session_13_abstraction/challenge/Challenge1.java
@@ -0,0 +1,4 @@
+package session_13_abstraction.challenge;
+
+public class Challenge1 {
+}
---
+++ src/main/java/session_15_oop_recap/practice/inheritance/Test2.java
@@ -0,0 +1,12 @@
+package session_15_oop_recap.practice.inheritance;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        Animal dog = new Dog("Rex", 2, "dog", "labrador", Size.LARGE);
+        Dog dog2 = (Dog) dog;
+        dog2.fetch();
+        dog2.bark();
+    }
+}
